b93815fea32bff2b6ff7b0019f8af7e8f7ff4e24
Colin Gilbert <colingilbert86@gmail.com>
2025-11-17 16:13:15 -0600

    Add egui

--- src/gui.rs
+++ src/gui.rs
@@ -1,14 +1,30 @@
+use noobwerkz::egui_tools::EguiRenderer;
 
-use yakui::{button, row, widgets::List, CrossAxisAlignment};
-pub fn user_gui() {
-        row(|| {
-        button("Not stretched");
-        let mut col = List::column();
-        col.cross_axis_alignment = CrossAxisAlignment::Stretch;
-        col.show(|| {
-            button("Button 1");
-            button("Button 2");
-            button("Button 3");
-        });
-    });
+
+pub fn user_gui(egui_renderer: &mut EguiRenderer) {
+        egui::Window::new("winit + egui + wgpu says hello!")
+            .resizable(true)
+            .vscroll(true)
+            .default_open(false)
+            .show(egui_renderer.context(), |ui| {
+                ui.label("Label!");
+
+                if ui.button("Button!").clicked() {
+                    println!("boom!")
+                }
+
+                ui.separator();
+                ui.horizontal(|ui| {
+                    ui.label(format!(
+                        "Pixels per point: {}",
+                        egui_renderer.context().pixels_per_point()
+                    ));
+                    if ui.button("-").clicked() {
+                        // state.scale_factor = (state.scale_factor - 0.1).max(0.3);
+                    }
+                    if ui.button("+").clicked() {
+                        // state.scale_factor = (state.scale_factor + 0.1).min(3.0);
+                    }
+                });
+            });
 }

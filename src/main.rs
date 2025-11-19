54552cd550975fcc2bdb4316beac74d2a2fcd103
Colin Gilbert <colingilbert86@gmail.com>
2025-11-19 14:32:36 -0600

    Update to match upstream

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use noobwerkz::callbacks::*;
 use noobwerkz_example::setup::*;
 use noobwerkz_example::update::*;
 use noobwerkz_example::gui::*;
-
+use noobwerkz_example::user_struct::*;
 fn main() {
+
     init_user_setup_callback(user_setup);
     init_user_update_callback(user_update);
     init_user_gui_callback(user_gui);
